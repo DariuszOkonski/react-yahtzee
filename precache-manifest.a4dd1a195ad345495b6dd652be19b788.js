8d27e707c35b3a24e1442a487dafa38074bad79b
Dariusz <darek200180@gmail.com>
2020-09-30 20:47:18 +0100

    Updates

---
+++ precache-manifest.a4dd1a195ad345495b6dd652be19b788.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9698760c235d2215b62d51b88dfcb4a7",
+    "url": "/react-yahtzee/index.html"
+  },
+  {
+    "revision": "2c7becf8aaddeabb72e2",
+    "url": "/react-yahtzee/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "028f8e2ca49cc0bf1516",
+    "url": "/react-yahtzee/static/js/2.fda1d702.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-yahtzee/static/js/2.fda1d702.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c7becf8aaddeabb72e2",
+    "url": "/react-yahtzee/static/js/main.796cd57a.chunk.js"
+  },
+  {
+    "revision": "0181714f6590c5574dcc",
+    "url": "/react-yahtzee/static/js/runtime-main.77ed4b78.js"
+  }
+]);
