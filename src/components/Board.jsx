45c55bdd2191c61a1aa9486513026cf7400ba109
Dariusz <darek200180@gmail.com>
2020-09-30 21:24:18 +0100

    add basic elements to Board component and style header

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -2,9 +2,23 @@ import React from 'react'
 
 const Board = () => {
     return ( 
-        <div className="board">
-            <h1>Board</h1>
-        </div>
+        <section className="board">
+            <div className="board__header">
+                <h1 className="header">Yahtzee!!!</h1>
+            </div>
+
+            <div className="board__dices">
+                <div className="dice">1</div>
+                <div className="dice">2</div>
+                <div className="dice">3</div>
+                <div className="dice">4</div>
+                <div className="dice">5</div>
+            </div>
+
+            <div className="board__button">
+                <button>2 Rolls Left</button>
+            </div>
+        </section>
      );
 }
  
