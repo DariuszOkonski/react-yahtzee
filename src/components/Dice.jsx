8096f32a99f6d058d3dc30ebdf83ca73162d35c1
Dariusz <darek200180@gmail.com>
2020-10-01 22:40:04 +0100

    make dices animate during rolling

--- src/components/Dice.jsx
+++ src/components/Dice.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { translateNumberToString } from '../auxiliaryFunctions/auxiliary';
+import { AppContext } from '../contexts/appContext';
 
 const Dice = ({dice}) => {
+    const { rolls } = useContext(AppContext);
     const diceNumber = translateNumberToString(dice);
 
     return ( 
-        <div className="dice">
+        <div className={`dice ${rolls && 'dice-rolling'}`}>
             <i className={`fas fa-dice-${diceNumber}`}></i>
         </div>
      );
